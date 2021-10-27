2339365a710628af5c38d37cde50725b74520f59
yleencc <1281540128@qq.com>
2021-10-27 00:57:38 +0000

    修正版权归属的布局不符合预期的问题

--- post.php
+++ post.php
@@ -146,11 +146,12 @@ $this->need('header.php');
 							<?php
                 $this->content();
 							?>
-							<hr>															<hr/>
+							<hr>												
 								<div style="padding: 1rem 1.2rem; margin-bottom: 1rem; background-color: rgba(170,170,200,0.1);">	
 		                                                <div>版权属于：月琳cc</div>
 	                                                        <div>本文链接：<?php $this->permalink(); ?></div>
-	                                                        <div>作品采用<a href="https://creativecommons.org/licenses/by/4.0/deed.zh">《知识共享署名-非商业性使用-相同方式共享 4.0 国际许可协议》</a>进行许可，转载请务必注明出处！</div>
+								<div>作品采用<a href="https://creativecommons.org/licenses/by/4.0/deed.zh">《知识共享署名-非商业性使用-相同方式共享 4.0 国际许可协议》</a>进行许可，转载请务必注明出处！</div>
+							</div>
 
 							
 							<ul>
